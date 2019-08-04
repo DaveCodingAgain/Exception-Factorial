9b8eb9edd096aaade9a9de0c8648af0d2a7a4592
David Buckner <DavidRossBuckner@outlook.com>
2019-08-04 08:26:48 -0600

    First Commit - MyMathFunctions Added

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Factorial
+{
+
+    public class MyMathFunctions
+    {
+        public static int Factorial(int value)
+        {
+            if (value < 0)
+            {
+                string s = String.Format(
+                    "Illegal negative argument to Factorial {0}", value);
+                throw new ArgumentException(s);
+            }
+
+
+            int factorial = 1;
+            do
+            {
+                factorial *= value;
+            } while (--value > 1);
+
+            return factorial;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
